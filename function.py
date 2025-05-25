6b2f0a9297ebb23f49fd4928d198fe3130d1cd5f
Grace444-arch <deogracoousssekilordgracejohns@gmail.com>
2025-05-25 16:13:04 +0300

    Add assignment two solution

---
+++ function.py
@@ -0,0 +1,8 @@
+def proudct_numbers(x1, y2):
+    """
+    This function takes two numbers as input and returns their product.
+    """
+    result =  x1 * y2
+    proudct_numbers =(20,20)
+    return result
+print(proudct_numbers(20,20))
